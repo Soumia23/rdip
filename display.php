f5e2beca4415200f0d776623288ac2a23499b3b5
Soumia23 <121710401065@gitam.in>
2020-06-26 23:06:34 +0530

    Submitted contain displayed on other page

---
+++ display.php
@@ -0,0 +1,15 @@
+<html>
+<?php
+  $Name = $_POST['name'];
+  $Number = $_POST['number'];
+  $Course = $_POST['course'];
+  $Description = $_POST['description'];
+  $Subject_Interested = $_POST['subject'];
+
+   echo("Name :" . $Name.<br/>);
+   echo ("Number :" . $Number.<br/>);
+   echo ("Course :" . $Course.<br/>);
+   echo ("Description :" . $Description.<br/>);
+   echo ( "Subject_Interested :" . $Subject_Interested.<br/>);
+?>
+</html>
